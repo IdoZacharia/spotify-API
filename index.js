e6735a10974655035f4bc1dee4c66f76e815911d
Ido-Zacharia <idozacharia@mail.tau.ac.il>
2025-10-15 16:30:38 +0300

    testing

--- index.js
+++ index.js
@@ -7,6 +7,30 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Spotify API!');
 });
 
+var client_id = 'CLIENT_ID';
+var client_secret = 'CLIENT_SECRET';
+
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    var token = body.access_token;
+  }
+});
+
+
+
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
